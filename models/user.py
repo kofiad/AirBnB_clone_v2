e4e9836e880010966097d9ad4c8d2c1fa69e3f35
qwertyriks <romeokumatse@gmail.com>
2024-04-17 23:27:47 +0000

    fixing user.py

--- models/user.py
+++ models/user.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
 """This is the user class"""
-from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
+import models
+from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
+    if models.is_type == "db":
+        __tablename__ = "users"
+
+        email = Column(String(128), nullable=False),
+        password = Column(String(128), nullable=False),
+        first_name = Column(String(128), nullable=False),
+        last_name = Column(String(128), nullable=False),
+    """This class defines a user by various attributes"""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
