3699b972e308d8b3733b78ebf61456b34041ba95
kofiad <kofiadaba@gmail.com>
2024-04-17 12:25:59 +0000

    minor code update

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,5 @@
---prepares a MySQL server for the project
+-- Prepares a MySQL Test server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS `hbnb_test`@`localhost` IDENTIFIED BY `hbnb_test_pwd`;
-GRANT All PRIVILEGES ON hbnb_test_db TO `hbnb_dev`@`localhost`;
-GRANT SELECT ON performance_schema TO `hbnb_dev`@`localhost`;
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
