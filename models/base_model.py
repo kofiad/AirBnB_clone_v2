9416bce146226b68369f6cbdaa31c9371cb7ffcf
qwertyriks <romeokumatse@gmail.com>
2024-04-17 22:47:52 +0000

    pycodestyle checks for base_model.py resolded

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,7 @@ import models
 
 Base = declarative_base()
 
+
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), unique=True, nullable=False, primary_key=True)
@@ -23,11 +24,15 @@ class BaseModel:
             self.updated_at = datetime.now()
         else:
             if 'updated_at' in kwargs:
-                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['updated_at'] = datetime.strptime(
+                    kwargs['updated_at'],
+                    '%Y-%m-%dT%H:%M:%S.%f'
+                    )
             if 'created_at' in kwargs:
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime.strptime(
+                    kwargs['created_at'],
+                    '%Y-%m-%dT%H:%M:%S.%f'
+                    )
             kwargs.pop('__class__', None)
             self.__dict__.update(kwargs)
 
