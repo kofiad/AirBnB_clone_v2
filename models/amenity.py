18101684bc0cffd9e77f141261dbe21398445d63
kofiad <kofiadaba@gmail.com>
2024-04-17 22:14:25 +0000

    work in progress

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 from models.place import place_amenity
 
 
