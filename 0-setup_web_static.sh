76aee8cc88efead7f6a47bfcc5ef7109fe851b9c
kofiad <kofiadaba@gmail.com>
2024-05-03 22:44:10 +0000

    work in progress

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -22,7 +22,7 @@ sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration
-sudo sed -i '/^\s*location \/hbnb_static/ s_^.*$_        alias /data/web_static/current/;_' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 # Restart Nginx
 sudo service nginx restart
